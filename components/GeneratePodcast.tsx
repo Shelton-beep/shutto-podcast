31fde5cff78fd5dd088f04f5c76c26bf8119a30e
Shelton Simbi <shltnsimbi@gmail.com>
2024-10-03 06:06:19 -0400

    Created Generate Podcast component

--- components/GeneratePodcast.tsx
+++ components/GeneratePodcast.tsx
@@ -1,5 +1,92 @@
-const GeneratePodcast = () => {
-  return <div>GeneratePodcast</div>;
+import { GeneratePodcastProps } from "@/types";
+import { Label } from "./ui/label";
+import { Textarea } from "./ui/textarea";
+import { Button } from "./ui/button";
+import { Loader } from "lucide-react";
+import { useState } from "react";
+import React from "react";
+
+//custom hook
+const useGeneratePodcast = ({
+  setAudio,
+  voiceType,
+  voicePrompt,
+  setAudioStorageId,
+}: GeneratePodcastProps) => {
+  //Logic for podcast generation
+  const [isGenerating, setIsGenerating] = useState(false);
+  const generatePodcast = async () => {
+    setIsGenerating(true);
+
+    setAudio("");
+
+    if (!voicePrompt) {
+      //todo:show error message
+      return setIsGenerating(false);
+    }
+
+    try {
+      /**
+       * const response = await getPodcastAudio({
+       * voice:voiceType,
+       * input:voicePrompt
+       * })
+       */
+    } catch (error) {
+      console.log("Error generating Podcast", error);
+      //todo:show error message
+      setIsGenerating(false);
+    }
+  };
+
+  return { isGenerating, generatePodcast };
+};
+
+const GeneratePodcast = (props: GeneratePodcastProps) => {
+  const { isGenerating, generatePodcast } = useGeneratePodcast(props);
+
+  return (
+    <div>
+      <div className="flex flex-col gap-2.5">
+        <Label className="text-16 font-bold text-white-1">
+          AI Prompt to generate Podcast
+        </Label>
+        <Textarea
+          className="input-class font-light focus-visible:ring-orange-1"
+          placeholder="Provide Text to generate podcast audio"
+          rows={5}
+          value={props.voicePrompt}
+          onChange={(e) => props.setVoicePrompt(e.target.value)}
+        />
+      </div>
+      <div className="mt-5 w-full max-w-[200px]">
+        <Button
+          type="submit"
+          className="text-16 bg-orange-1 py-4 font-bold text-white-1"
+        >
+          {isGenerating ? (
+            <>
+              Generating
+              <Loader size={20} className="animate-spin ml-2" />
+            </>
+          ) : (
+            <>Generate</>
+          )}
+        </Button>
+      </div>
+      {props.audio && (
+        <audio
+          controls
+          src={props.audio}
+          autoPlay
+          className="mt-5"
+          onLoadedMetadata={(e) =>
+            props.setAudioDuration(e.currentTarget.duration)
+          }
+        />
+      )}
+    </div>
+  );
 };
 
 export default GeneratePodcast;
