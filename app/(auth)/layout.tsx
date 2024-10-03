80c0df04f7bdc83a91fb99a206fee3c5e9d2b36b
Shelton Simbi <shltnsimbi@gmail.com>
2024-10-02 22:25:44 -0400

    created authentication and clerk setup

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,5 +1,19 @@
+import Image from "next/image";
+
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return <main>{children}</main>;
+  return (
+    <main className="relative h-screen w-full">
+      <div className="absolute size-full">
+        <Image
+          src={"/images/bg-img.png"}
+          alt="background"
+          fill
+          className="size-full"
+        />
+      </div>
+      {children}
+    </main>
+  );
 };
 
 export default AuthLayout;
---
+++ convex/auth.config.ts
@@ -0,0 +1,9 @@
+const authConfig =  {
+    providers:[
+        {domain:"https://capital-bluebird-11.clerk.accounts.dev",
+            applicationID:"convex"
+        }
+    ]
+}
+
+export default authConfig;
